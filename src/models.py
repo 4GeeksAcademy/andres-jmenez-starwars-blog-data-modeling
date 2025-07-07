9007d5b27345050faee1e15d192360cc53fe52d6
Andres Jimenez <ajthecoder1@gmail.com>
2025-07-07 17:40:29 +0000

    Starwars blog

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -16,4 +16,47 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Characters(db.Model):
+    id: Mapped[int] = mapped_column(primary_key = True)
+    name: Mapped[str] = mapped_column(String(120))
+    eye_color: Mapped[str] = mapped_column()
+    birth_year: Mapped[str] = mapped_column(String(10))
+    character_favorites: Mapped["Favorites"] = relationship(back_populates = "favorites_characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+        }
+    
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key = True)
+    name: Mapped[str] = mapped_column(String(120))
+    terrain: Mapped[str] = mapped_column()
+    population: Mapped[str] = mapped_column()
+    planet_favorites: Mapped["Favorites"] = relationship(back_populates = "favorites_planets")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name":self.name,
+            "terrain": self.terrain,
+            "population": self.population, 
+        }
+    
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    character_id: Mapped[int] = mapped_column(ForeignKey("characters.id"))
+    planet_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
+    favorites_characters: Mapped[list["Characters"]] = relationship(back_populates = "character_favorites")
+    favorites_planets: Mapped[list["Planets"]] = relationship(back_populates = "planet_favorites")
+
+    def serialize(self):
+        return {
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
         }
